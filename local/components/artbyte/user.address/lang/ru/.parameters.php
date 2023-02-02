bf6051e6601b29f534e4069fed45cb25b0f6fc69
Dimitry Fominykh <a@webheads.ru>
2023-02-02 21:22:35 +0100

    feat/new_component_v1 | Закинул базовый шаблон компонента из докоументации битрикса (структура папок и фалов, тестовое содержание)

---
+++ local/components/artbyte/user.address/lang/ru/.parameters.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$MESS["EXAMPLE_ARTBYTE_PROP_SETTINGS"] = "Выбор инфоблока и разделов";
+$MESS["EXAMPLE_ARTBYTE_PROP_IBLOCK_TYPE"] = "Тип инфоблока";
+$MESS["EXAMPLE_ARTBYTE_PROP_IBLOCK_ID"] = "Инфоблок";
+$MESS["EXAMPLE_ARTBYTE_PROP_SECTION_IDS"] = "ID разделов через запятую";
